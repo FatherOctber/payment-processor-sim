8a928ca47e0c898ee38d0c15947982dafa4468c1
Nikita Mishnev <nmishnev@openwaygroup.com>
2019-02-21 19:30:01 +0300

    supported redis in docker-compose

--- tokenizer/src/main/java/com/fatheroctober/ppsim/tokenizer/TokenizerApplication.java
+++ tokenizer/src/main/java/com/fatheroctober/ppsim/tokenizer/TokenizerApplication.java
@@ -1,9 +1,14 @@
 package com.fatheroctober.ppsim.tokenizer;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan({"com.fatheroctober.ppsim.tokenizer", "com.fatheroctober.ppsim.common"})
 @SpringBootApplication
 public class TokenizerApplication {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = SpringApplication.run(TokenizerApplication.class, args);
+    }
 }
